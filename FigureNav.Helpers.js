ea8019388cd36e7c1d4a58d99ddfcbd5907be6a3
Ryan Gelow <ryangelow@Offset-MacBook-William.local>
2020-08-27 18:03:10 -0700

    adjusted search data to config.links and also added Levenshtein distance formula for getting close search results accounting for typos.

--- FigureNav.Helpers.js
+++ FigureNav.Helpers.js
@@ -90,11 +90,15 @@ FigureNav.Helpers = {
    * @param {Array} searchData
    */
   renderSearchResult: function (input, searchData) {
-    const tableLength = searchData.length > 10 ? 10 : searchData.length;
+    // const searchTable = searchData.links.concat(searchData.search);
+    const searchTable = searchData.links;
+    console.log(searchTable);
+    let searchFilter = searchTable.filter(search => search.key != "dashboard");
+    let searchResults = !searchFilter ? searchFilter : searchTable;
+    let tableLength = searchResults.length > 10 ? 10 : searchResults.length;
     FigureNav.Components.Search.List.empty();
     const SearchEnd = FigureNav.Components.Search.AdvancedSearch();
     SearchEnd.addClass('advanced-search');
-    // console.log(SearchEnd)
     
     if (input.length <= 0) {
       FigureNav.Components.Search.List.append(SearchEnd);
@@ -103,10 +107,71 @@ FigureNav.Helpers = {
 
     const data = [];
     for (i = 0; i < tableLength; i++) {
-      data.push(searchData[i]);
+      data.push(searchResults[i]);
     }
 
-    data.forEach(function (tableData) {
+    console.log(data)
+    const results = [];
+    const filter = input.toUpperCase();
+    console.log(filter, input)
+    for(let i = 0; i < data.length; i++) {
+      const comp = data[i].label;
+      var validity = 0;
+      if (comp.toUpperCase().indexOf(filter) > -1) {
+        validity = 1;
+      } else {
+        validity = checkSimilarity(comp, filter)
+      }
+      // Here is where we adjust the percent of closeness for input and resulting search content
+      if(validity > .45) {
+        results.push(data[i])
+        console.log(validity);
+      }
+    }
+
+    // Levenshtein distance formula for similarity in character structure, so we can account for misspellings
+    function checkSimilarity(s1, s2) {
+      var longer = s1;
+      var shorter = s2;
+      if (s1.length < s2.length) {
+        longer = s2;
+        shorter = s1;
+      }
+      var longerLength = longer.length;
+      if (longerLength == 0) {
+        return 1.0;
+      }
+      return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+    }
+
+    function editDistance(s1, s2) {
+      s1 = s1.toLowerCase();
+      s2 = s2.toLowerCase();
+
+      var costs = new Array();
+      for (var i = 0; i <= s1.length; i++) {
+        var lastValue = i;
+        for (var j = 0; j <= s2.length; j++) {
+          if (i == 0)
+            costs[j] = j;
+          else {
+            if (j > 0) {
+              var newValue = costs[j - 1]; 
+              if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                newValue = Math.min(Math.min(newValue, lastValue),
+                  costs[j]) + 1;
+              costs[j - 1] = lastValue;
+              lastValue = newValue;
+            }
+          }
+        }
+        if (i > 0)
+          costs[s2.length] = lastValue;
+      }
+      return costs[s2.length];
+    }
+
+    results.forEach(function (tableData) {
       const row = FigureNav.Components.Search.ListItem(
         tableData.key,
         tableData.label
